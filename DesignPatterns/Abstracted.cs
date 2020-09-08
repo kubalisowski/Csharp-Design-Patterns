1bea928af791bb7662df22dac8799e6eaec985d9
Kuba <kubalisowski94@gmail.com>
2020-09-08 14:04:28 +0200

    Add project files.

---
+++ DesignPatterns/Abstracted.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns
+{
+    // Base class
+    public abstract class Abstracted
+    {
+        // Abstract method
+        public abstract void VoidAbstract();
+        public abstract string SayAbstract(string message);
+
+        //Non-Abstract
+        public void NonAbstract() 
+        {
+            Console.WriteLine("I am not abstract void method.");
+        }
+    }
+
+    // Child (deriver) class
+    // must deriver abstract methods
+    public class Child : Abstracted
+    {
+        public override void VoidAbstract()
+        {
+            Console.WriteLine("I am abstract void method.");
+        }
+
+        public override string SayAbstract(string message)
+        {
+            return message;
+        }
+    }
+
+    // Variable abstract class
+    public abstract class Var
+    {
+        // PROTECTED: The type or member can be accessed only by code in the same class, or in a class that is derived from that class.
+        protected string _var;
+        public abstract string abstractedVariable
+        {
+            get;
+            set;
+        }
+    }
+
+    public class ChildVar : Var
+    {
+        public override string abstractedVariable
+        {
+            get
+            {
+                return _var;
+            }
+            set
+            {
+                _var = value;
+            }
+        }
+    }
+}
---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /// ABSTRACTION ///
+            // Class Program is a 'driver' class for Abstracted
+            Child child = new Child();
+            Console.WriteLine(child.SayAbstract("I am abstract string method."));
+            child.VoidAbstract();
+            child.NonAbstract();
+
+            // Variable class
+            ChildVar var = new ChildVar();
+            var.abstractedVariable = "I am derived abstracted variable";
+            Console.WriteLine(var.abstractedVariable);
+            ///////////////////
+            
+        }
+    }
+}
